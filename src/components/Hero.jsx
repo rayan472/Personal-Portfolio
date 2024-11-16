20116b6d8489d542f62743759ec1463b9ebe33bb
rayan472 <rayanaly000@gmial.com>
2024-11-16 18:42:47 +0500

    change the path of resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,8 +30,8 @@ const Hero = () => {
             onClick={() => navigate('/contact')}>Contact With Me</button>
 
           <a
-            href="/Rayan (1).pdf" // File path in the public folder
-            download="Rayan_Aly_Resume.pdf" // Name of the downloaded file
+            href="/Rayan (1).pdf" 
+            download="Resume.pdf" 
             className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full cursor-pointer'>
             Resume
