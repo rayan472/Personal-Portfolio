72819757e569f8e6835315b711c66ddd84abd234
rayan472 <rayanaly000@gmial.com>
2024-11-16 19:18:21 +0500

    add the resume file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ import About from './About';
 import Service from './Service';
 import Projects from './Projects';
 import Contact from './Contact';
+import Resume from '../assets/Rayan.pdf'
 
 const Hero = () => {
   const navigate = useNavigate();
@@ -30,8 +31,8 @@ const Hero = () => {
             onClick={() => navigate('/contact')}>Contact With Me</button>
 
           <a
-            href="/Rayan (1).pdf" 
-            download="Resume.pdf" 
+            href={Resume} 
+            download="Rayan.pdf" 
             className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full cursor-pointer'>
             Resume
